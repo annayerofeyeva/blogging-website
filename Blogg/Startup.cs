70fb7125921f81108f460809d8279a3fce242511
Anna <jerofejeva.anna@yahoo.com>
2015-01-04 18:07:21 +0000

    Restructured the first sketch

---
+++ Blogg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blogg.Startup))]
+namespace Blogg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
